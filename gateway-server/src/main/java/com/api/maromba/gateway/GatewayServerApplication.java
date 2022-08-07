0a6da125da2d8e22efe2394e913944f489bead6f
YuriLeao <ydomingosleao@gmail.com>
2022-08-06 21:14:02 -0300

    Ajuste para a aplicação do gateway iniciar depois das demais, para conseguir montar o swagger corretamente.

--- gateway-server/src/main/java/com/api/maromba/gateway/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/api/maromba/gateway/GatewayServerApplication.java
@@ -1,12 +1,15 @@
 package com.api.maromba.gateway;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(90);
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
