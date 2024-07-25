79d136e1eaa9f015de60e350fe79eb05881bade5
YuriLeao <ydomingosleao@gmail.com>
2024-07-25 17:43:28 -0300

    Adjustment to call company-service by feign

--- user-service/src/main/java/com/api/maromba/user/proxy/CompanyProxy.java
+++ user-service/src/main/java/com/api/maromba/user/proxy/CompanyProxy.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "company-service")
 public interface CompanyProxy {
 	
-	@GetMapping("/getById/{id}")
+	@GetMapping("company-service/getById/{id}")
 	public ResponseEntity<Object> getById(@PathVariable(value = "id") UUID id);
 		
 }
