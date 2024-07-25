8e123e36e7b57c63d87f7387c3ea338cc3fa5d49
YuriLeao <ydomingosleao@gmail.com>
2024-07-25 12:56:26 -0300

    Correction of the warnins of the run.

---
+++ eureka-server/src/main/java/com/api/maromba/eureka/configuration/CacheConfiguration.java
@@ -0,0 +1,27 @@
+package com.api.maromba.eureka.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+    @Bean
+    Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterWrite(10, java.util.concurrent.TimeUnit.MINUTES);
+    }
+
+    @Bean
+    CaffeineCacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
