43974678bda9f7184681aa2f0d4ca72ae81d4d80
YuriLeao <ydomingosleao@gmail.com>
2024-07-25 12:30:39 -0300

    Removing annotation of Repository
    Because ins't more necessary.

--- company-service/src/main/java/com/api/maromba/company/repositories/CompanyRepository.java
+++ company-service/src/main/java/com/api/maromba/company/repositories/CompanyRepository.java
@@ -3,11 +3,9 @@ package com.api.maromba.company.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.api.maromba.company.models.CompanyModel;
 
-@Repository
 public interface CompanyRepository extends JpaRepository<CompanyModel, UUID>{
 	
 	boolean existsByName(String name);
--- workout-service/src/main/java/com/api/maromba/workout/repositories/WorkoutItemRepository.java
+++ workout-service/src/main/java/com/api/maromba/workout/repositories/WorkoutItemRepository.java
@@ -3,11 +3,8 @@ package com.api.maromba.workout.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.api.maromba.workout.models.WorkoutItemModel;
 
-@Repository
 public interface WorkoutItemRepository extends JpaRepository<WorkoutItemModel, UUID>{
 
 }
